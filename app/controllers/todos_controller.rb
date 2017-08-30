9255284a5c419b9806755c5a1cf390993d3cc8d2
Spencer197 <spncr@protonmail.com>
2017-08-30 14:30:18 +0000

    Extract redundancies and refactoring

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
   
   def new
     @todo = Todo.new
@@ -15,15 +16,15 @@ class TodosController < ApplicationController
   end
   
   def show
-    @todo = Todo.find(params[:id])#Finds a specific Todo entry.
+    #See def set_todo under private
   end
   
   def edit
-    @todo = Todo.find(params[:id])
+    #See def set_todo under private
   end
   
   def update
-   @todo = Todo.find(params[:id])#Creates a new Todo object.
+    #See def set_todo under private
     if @todo.update(todo_params)#Saves/updates form input to the database. If successful,
       flash[:notice] = "Todo was successfully updated."#Adds 'successful' flash message.
       redirect_to todo_path(@todo)#Redirects input to the Todo show page.
@@ -37,13 +38,17 @@ class TodosController < ApplicationController
   end
   
   def destroy
-    @todo = Todo.find(params[:id])
+    #See def set_todo under private
     @todo.destroy
     flash[:notice] = "Todo was deleted successfully."
     redirect_to todos_path
   end
   
   private
+  
+  def set_todo
+    @todo = Todo.find(params[:id])# Finds a specific Todo object for each of the show, edit, update, & destroy methods.  This line was "extracted away" from these methods.
+  end
   
   def todo_params# Adds 'strong parameters' which white lists the kinds of parameters (name, description) recieved from the Todo form.
     params.require(:todo).permit(:name, :description)
